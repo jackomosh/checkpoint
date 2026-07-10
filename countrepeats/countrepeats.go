c40de4abd19e1193b48a805a369a26ebe37c6d71
jackomosh <jackomosh6@gmail.com>
2026-07-10 14:54:00 +0300

    fixed countrepeats

--- countrepeats/countrepeats.go
+++ countrepeats/countrepeats.go
@@ -1,45 +1,3 @@
-// package main
-
-// import "fmt"
-
-// func CountRepeats(s string) string {
-// 	if s == "" {
-// 		return s
-// 	}
-
-// 	runes := []rune(s)
-// 	result := ""
-// 	counter := 1
-
-// 	for i := 0; i < len(runes); i++ {
-// 		// Increment count if the next character matches
-// 		if i+1 < len(runes) && runes[i] == runes[i+1] {
-// 			counter++
-// 			continue
-// 		}
-
-// 		// Append character directly
-// 		result += string(runes[i])
-
-// 		// Append count using fmt if character repeated
-// 		if counter > 1 {
-// 			result += fmt.Sprintf("%d", counter)
-// 		}
-// 		counter = 1
-// 	}
-
-// 	return result
-// }
-
-// func main() {
-// 	fmt.Println(CountRepeats("ABCABC"))       // Output: ABCABC
-// 	fmt.Println(CountRepeats("AAABBC"))       // Output: A3B2C
-// 	fmt.Println(CountRepeats("JjjJohhnnnNn")) // Output: Jj3Joh2n3Nn
-// 	fmt.Println(CountRepeats("     ")) 
-// 	fmt.Println(CountRepeats("  Jaaccccckk   "))         // Output:  5
-// }
-
-
 package main
 
 import "fmt"
@@ -48,20 +6,28 @@ func CountRepeats(s string) string {
 	if s == "" {
 		return s
 	}
+
 	runes := []rune(s)
 	result := ""
 	counter := 1
+
 	for i := 0; i < len(runes); i++ {
-		if i + 1 < len(runes) && runes[i] == runes[i+1] {
+		// Increment count if the next character matches
+		if i+1 < len(runes) && runes[i] == runes[i+1] {
 			counter++
 			continue
 		}
+
+		// Append character directly
 		result += string(runes[i])
-		if counter > 0{
-			result += fmt.Sprintf("%d", counter) 
+
+		// Append count using fmt if character repeated
+		if counter > 1 {
+			result += fmt.Sprintf("%d", counter)
 		}
 		counter = 1
 	}
+
 	return result
 }
 
