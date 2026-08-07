713de223f08eecf58baccfd4be39640a77ec2eb1
jackomosh <jackomosh6@gmail.com>
2026-08-07 09:23:07 +0300

    union

--- union/union.go
+++ union/union.go
@@ -8,16 +8,16 @@ import (
 func main() {
 
 	if len(os.Args) != 3 {
-		fmt.Println("Invalid Operation Provide atleast two args")
+		fmt.Println()
 		return
 	}
 
 	str1 := os.Args[1]
 	str2 := os.Args[2]
 	combined := str1 + str2
+	var newString []rune
 
 	seen := make(map[rune]bool)
-	var newString []rune
 
 	for _, ch := range combined {
 		if !seen[ch] {
