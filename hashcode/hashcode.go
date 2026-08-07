3f45ca6fb275f6ccbc436bc71c6ace478b07071b
jackomosh <jackomosh6@gmail.com>
2026-08-07 11:20:11 +0300

    function that turns camelcase syntax to snakecase

--- hashcode/hashcode.go
+++ hashcode/hashcode.go
@@ -3,16 +3,18 @@ package main
 import "fmt"
 
 func HashCode(dec string) string {
+
 	size := len(dec)
-	res := ""
+	result := ""
+
 	for _, ch := range dec {
-		hashedValue := int(ch) + size % 127
+		hashedValue := int(ch) + size%127
 		if hashedValue < 32 {
 			hashedValue += 33
 		}
-		res += string(rune(hashedValue))
+		result += string(rune(hashedValue))
 	}
-	return res
+	return result
 }
 
 func main() {
--- printmemory/printmemory.go
+++ printmemory/printmemory.go
@@ -4,17 +4,21 @@ import "github.com/01-edu/z01"
 
 func PrintMemory(arr [10]byte) {
 	hexDigits := "0123456789abcdef"
+
 	for i := 0; i < len(arr); i++ {
 		firstDigit := arr[i] / 16
 		secondDigit := arr[i] % 16
+
 		z01.PrintRune(rune(hexDigits[firstDigit]))
 		z01.PrintRune(rune(hexDigits[secondDigit]))
+
 		if i == 3 || i == 7 || i == 9 {
 			z01.PrintRune('\n')
 		} else {
 			z01.PrintRune(' ')
 		}
 	}
+
 	for i := 0; i < 10; i++ {
 		if arr[i] >= 32 && arr[i] <= 126 {
 			z01.PrintRune(rune(arr[i]))
