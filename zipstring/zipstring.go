3d0d755f9195513d5a3f4f389c13b949d2d1175b
jackomosh <jackomosh6@gmail.com>
2026-08-17 22:16:32 +0300

    fix: used an int to string helper function in order to avoid fmt

--- zipstring/zipstring.go
+++ zipstring/zipstring.go
@@ -1,9 +1,5 @@
 package main
 
-import (
-	"fmt"
-)
-
 func ZipString(s string) string {
 
 	if s == "" {
@@ -14,20 +10,36 @@ func ZipString(s string) string {
 	result := ""
 	count := 1
 
-	for i := 1; i < len(runes); i++ {
-		if runes[i] == runes[i-1] {
+	for i := 1; i <= len(runes); i++ {
+		if i < len(runes) && runes[i] == runes[i-1] {
 			count++
 		} else {
-			result += fmt.Sprintf("%d%c", count, runes[i-1])
+			result += intToString(count) + string(runes[i-1])
 			count = 1
 		}
 	}
-	result += fmt.Sprintf("%d%c", count, runes[len(runes)-1])
+	return result
+}
+
+func intToString(n int) string {
+
+	if n == 0 {
+		return "0"
+	}
+
+	result := ""
+
+	for n > 0 {
+		digit := n % 10
+		result = string(rune('0'+digit)) + result
+		n = n / 10
+	}
 	return result
 }
 
 func main() {
-	fmt.Println(ZipString("YouuungFellllas"))
-	fmt.Println(ZipString("Thee quuick browwn fox juumps over the laaazy dog"))
-	fmt.Println(ZipString("Helloo Therre!"))
+	println(ZipString("YouuungFellllas"))
+	println(ZipString("Thee quuick browwn fox juumps over the laaazy dog"))
+	println(ZipString("Helloo Therre!"))
+	println(ZipString("Haaaaaaaaaa"))
 }
